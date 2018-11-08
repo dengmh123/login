e15a011f738afb56e8f0396ab3056418db9f2633
dengmh <dengmh123@163.com>
2018-11-08 09:57:26 +0800

    更新页面UI和添加css

--- src/main/java/com/dmh/config/WebSecurityConfig.java
+++ src/main/java/com/dmh/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,7 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {//扩展Spr
         http
                 .headers().frameOptions().disable()
                 .and().authorizeRequests()
-                .antMatchers("/registry").permitAll() // 注册请求按钮不需要验证
+                .antMatchers("/register").permitAll() // 注册请求按钮不需要验证
+                .antMatchers("/static/**","/images/**").permitAll()//静态资源允许访问
                 .antMatchers("/zhuce").permitAll() // 注册请求页面不需要验证
                 .anyRequest().authenticated()//所有的请求需要认证即登陆后才能访问
                 .and().formLogin().loginPage("/sign_in").loginProcessingUrl("/login")
